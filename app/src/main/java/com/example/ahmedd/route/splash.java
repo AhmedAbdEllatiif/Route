b14ff05f8526ff22e37a10405aea42a23b7634f1
Ahmed <ahmedmohamedaneng@gmail.com>
2018-08-28 11:29:27 +0200

    splash

--- app/src/main/java/com/example/ahmedd/route/splash.java
+++ app/src/main/java/com/example/ahmedd/route/splash.java
@@ -1,13 +1,26 @@
 package com.example.ahmedd.route;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class splash extends AppCompatActivity {
 
+    Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+       handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this, Login.class);
+                startActivity(intent);
+
+            }
+        },3000);
     }
 }
