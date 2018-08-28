da59322343a5e582079bb98780a16ad558f9137e
Ahmed <ahmedmohamedaneng@gmail.com>
2018-08-28 14:21:45 +0200

    Button Next

--- app/src/main/java/com/example/ahmedd/route/Login.java
+++ app/src/main/java/com/example/ahmedd/route/Login.java
@@ -1,13 +1,29 @@
 package com.example.ahmedd.route;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Survey.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
