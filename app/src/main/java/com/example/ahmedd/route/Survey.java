6606dd88827d4911f56a0119f8b749c589bc10d5
Ahmed <ahmedmohamedaneng@gmail.com>
2018-08-28 16:53:35 +0200

    Information_activity

--- app/src/main/java/com/example/ahmedd/route/Survey.java
+++ app/src/main/java/com/example/ahmedd/route/Survey.java
@@ -1,13 +1,32 @@
 package com.example.ahmedd.route;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class Survey extends AppCompatActivity {
+
+    TextView txtBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
+
+    txtBack = (TextView) findViewById(R.id.txtBack);
+
+    txtBack.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(Survey.this,Login.class);
+            startActivity(intent);
+        }
+    });
+
+
     }
 }
