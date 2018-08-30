6f1de934538d44e2ed3047de4444b29249526d9f
Ahmed <ahmedmohamedaneng@gmail.com>
2018-08-30 15:10:02 +0200

    gender_upadte

--- app/src/main/java/com/example/ahmedd/route/Survey.java
+++ app/src/main/java/com/example/ahmedd/route/Survey.java
@@ -4,13 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
 public class Survey extends AppCompatActivity {
 
     TextView txtBack;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,7 @@ public class Survey extends AppCompatActivity {
         setContentView(R.layout.activity_survey);
 
     txtBack = (TextView) findViewById(R.id.txtBack);
+    spinner = (Spinner) findViewById(R.id.spinner);
 
     txtBack.setOnClickListener(new View.OnClickListener() {
         @Override
@@ -27,6 +31,12 @@ public class Survey extends AppCompatActivity {
         }
     });
 
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Survey.this,android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.genders));
+
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(myAdapter);
 
     }
 }
