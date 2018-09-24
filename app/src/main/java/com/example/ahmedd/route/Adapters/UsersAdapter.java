d84cd767378250cbd05e84e3d59249c6687ffe4f
Ahmed <ahmedmohamedaneng@gmail.com>
2018-09-24 14:14:51 +0200

    DataBase_works
    Button next add data in the DataBase.
    
    Button show intent activity UsersView to show All data in recyclerView

---
+++ app/src/main/java/com/example/ahmedd/route/Adapters/UsersAdapter.java
@@ -0,0 +1,33 @@
+package com.example.ahmedd.route.Adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UsersAdapters extends RecyclerView.Adapter<UsersAdapters.ViewHolder> {
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
